1eaff305510accce1142ce7dab6d12b39aaaa4e3
Vishal <vishalkumar916171@gmail.com>
2026-07-19 07:49:43 +0530

    React-Router-dom/basic

--- 07-React-Router-Dom/React-Router-Dom/src/App.jsx
+++ 07-React-Router-Dom/React-Router-Dom/src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
-
      </Routes>
 
     </div>
